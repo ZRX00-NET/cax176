fe08a13c905c2fdd093b5bcdbf6cf638113ba640
ZRX00-NET <zhirongxu00@gmail.com>
2026-07-06 20:09:31 -0400

    lab2

---
+++ Challenge.py
@@ -0,0 +1,11 @@
+age = float(input("What is your age: "))
+
+if age < 18:
+    print("Damn no!")
+elif 18 <= age <= 20:
+    print("Come in, but no drinks for you.")
+elif age = str:
+    print("Why do your age comes with alpabets?")
+else:
+    print("Welcome in! Drink drink drink.")
+    
---
+++ types_and_vars.py
@@ -0,0 +1,19 @@
+name = "Lauren"
+age = 199
+height = 4.65
+print("Hello, my name is", name, ".\nI am", age, "years old and", height, "meters tall.")
+# Age calculation
+print("In 5 years, I will be", age + 5, "years old.")
+# Area of rectangle
+width =  5.5
+height = 2
+print("The area of a 5.5 width and 2.0 height rectangle is", width*height, ".")
+#Two different arithmetic operations
+print(7//9+3-1*2)
+print(6**7-4**5)
+#String Concatentaion and repetition
+water = "Water!"
+food = "Food?"
+WF = water + " " + food + " "
+print(WF*7)
+
