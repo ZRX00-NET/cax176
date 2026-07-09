a227de94611ca496a50b937408568bf67e3048bd
ZRX00-NET <zhirongxu00@gmail.com>
2026-07-08 20:22:26 -0400

    pratices

--- Challenge.py
+++ Challenge.py
@@ -1,11 +1,13 @@
-age = float(input("What is your age: "))
+age = int(input("What is your age: "))
+
+while type(age) != int:
+    print(input("Enter age number: "))
+    break
 
 if age < 18:
     print("Damn no!")
 elif 18 <= age <= 20:
     print("Come in, but no drinks for you.")
-elif age = str:
-    print("Why do your age comes with alpabets?")
 else:
     print("Welcome in! Drink drink drink.")
     
