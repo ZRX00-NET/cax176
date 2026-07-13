289891c1a359bee47cb780544cbd6c654dc997fb
ZRX00-NET <zhirongxu00@gmail.com>
2026-07-12 23:52:50 -0400

    first completed

--- new_grade.py
+++ new_grade.py
@@ -1,27 +1,27 @@
+a = range(90, 101)
+b = range(80, 90)
+c = range(70, 80)
+d = range(60, 70)
+f = (0, 60)
+
 name = input("What if your name: ")
 print("nice to meet you", name)
 grade = int(input("what is your grade: "))
 
-a = [90, 100]
-b = [80, 89]
-c = [70, 79]
-d = [60, 69]
-f = [0, 59]
-
-if grade == a:
+if grade in a:
     print("Your grade is: A")
-elif grade == b:
+elif grade in b:
     print("Your grade is: B")
-elif grade == c:
+elif grade in c:
     print("Your grade is: C")
-elif grade == d:
+elif grade in d:
     print("Your grade is: D")
-elif grade == f:
+elif grade in f:
     print("Your grade is: D")
 else:
     print("Enter a valid grade.")
 
-if grade == a or grade == b or grade == c:
+if grade in a or grade in b or grade in c:
     print("Great Job!")
-elif grade == d or grade == f:
+elif grade in d or grade in f:
     print("Work harder!")
