f3e01c62d15f95f25c01926f9521806dac5e0775
ZRX00-NET <zhirongxu00@gmail.com>
2026-07-12 23:45:46 -0400

    save

--- Challenge.py
+++
@@ -1,9 +0,0 @@
-age = int(input("What is your age: "))
-
-if age < 18:
-    print("Damn no!")
-elif 18 <= age <= 20:
-    print("Come in, but no drinks for you.")
-else:
-    print("Welcome in! Drink drink drink.")
-    
