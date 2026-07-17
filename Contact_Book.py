99076749d11a762151187b93b01798ff082edffb
ZRX00-NET <zhirongxu00@gmail.com>
2026-07-17 01:54:55 -0400

    review list

--- Contact_Book.py
+++ Contact_Book.py
@@ -1,17 +1,31 @@
 ContactList = {}
 
-def AddNewContact(name, Pnumber):
-    #name = input("Please enter contact name: ")
-    #Pnumber = input("Please enter the phone number: ")
-    if name not in ContactList and name not in ContactList:
-        ContactList.update({name: Pnumber})
-        print("Contact added.")
-    else:
-        print("Do Not Enter Duplicate Entry.")
+def AddContact():
+    def AddNewContact(name, Pnumber):
+        #name = input("Please enter contact name: ")
+        #Pnumber = input("Please enter the phone number: ")
+        if name not in ContactList:
+            ContactList.update({name: Pnumber})
+            print("Contact added.")
+        else:
+            print("Do Not Enter Duplicate Entry.")
 
-nameE = input("Please enter contact name: ")
-PnumberE = input("Please enter the phone number: ")
-AddNewContact(name = nameE, Pnumber = PnumberE)
+    nameE = input("Please enter contact name: ")
+    PnumberE = input("Please enter the phone number: ")
+    AddNewContact(name = nameE, Pnumber = PnumberE)
+
+
+def ViewAllContact():
+    if len(ContactList) == 0:
+        print("Empty Contact List.")
+    else:
+        for name, Pnumber in ContactList.items():
+            print("Contact: ", name, Pnumber, end = "\n")
+                    
+                    
+
+AddContact()
+ViewAllContact()
 
     
             
